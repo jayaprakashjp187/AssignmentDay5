51c9f57d7934cf39e2b479a340bb2526bda482f6
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-14 13:07:14 +0530

    modifying the code

--- AssignmentDay5/FlipCoin.cs
+++ AssignmentDay5/FlipCoin.cs
@@ -10,17 +10,28 @@ namespace AssignmentDay5
     {
         public static void NumberOfTimesFlipCoin(int num)
         {
-            Random rnd = new Random();
-            int randomInPut=rnd.Next(0,1);
-            if(randomInPut<0.5)
+            Random Flip = new Random();
+            int randomInPut=Flip.Next(0,1);
+            int heads = 0;
+            int tails = 0;
 
+            for (int i = 0; i < num; i++)
             {
-                Console.WriteLine("tails");
+                if (Flip.NextDouble() < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
             }
-            else
-            {
-                Console.WriteLine("heads");
-            }
+
+            double headsPercentage = ((double)heads / num) * 100;
+            double tailsPercentage = ((double)tails / num) * 100;
+
+            Console.WriteLine("heads " + headsPercentage.ToString("0.00") + "percentage");
+            Console.WriteLine("tails " + tailsPercentage.ToString("0.00") + "percentage");
 
         }
     }
--- AssignmentDay5/LeapYear.cs
+++ AssignmentDay5/LeapYear.cs
@@ -10,7 +10,11 @@ namespace AssignmentDay5
     {
         public static void checkingLeapYearOrNot(int year) 
         {
-            if(year%4==0)
+            if (year<999 || year>10000)
+            {
+                Console.WriteLine("ivalid a year please enter a four digits of year");
+            }
+            else if(year%4==0)
             {
                 Console.WriteLine(year +"is aleap year");
             }
